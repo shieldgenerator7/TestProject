7ac280b7790257c860a33ddae7bcd1947a2403d3
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-06-27 15:25:13 -0400

    hi caleb its you from the future

--- Assets/scriptMe.cs
+++ Assets/scriptMe.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scriptMe : MonoBehaviour
+public class scriptMe : MonoBeshaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Strt()
     {
+        int i = 0;
         
     }
 
     // Update is called once per frame
-    void Update()
     {
         
     }
